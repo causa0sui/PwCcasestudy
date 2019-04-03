48dff6b44e586ac56b97ae55a3b39c2afd193f7d
Pavlo Melnyk <p.melnyk@student.uw.edu.pl>
2019-04-03 21:44:21 +0200

    Wywaliłem zbędny kod, gini ten sam 0.441

--- model.R
+++ model.R
@@ -30,9 +30,6 @@ test <- df[-train_ind, ]
 
 #Step 1 - Univariate analysis
 
-table(df$Age)
-hist(df$Age)
-
 colname <- c("GEO_region","Age","Job_type","Home_status","Car_status","Household_children","Monthly_Income","Monthly_Spendings",
              "Credit_purpose","Credit_amount","Number_of_installments","NotionalValue_t0","NotionalValue_lag1",
              "NotionalValue_lag2","NotionalValue_lag3","NotionalValue_lag4","NotionalValue_lag5","NotionalValue_lag6",
@@ -50,21 +47,6 @@ colname <- c("GEO_region","Age","Job_type","Home_status","Car_status","Household
 #plot(df$Household_children, df$DefFlag)
 #plot(df$NotionalValue_t0, df$DefFlag)
 
-library(ggplot2)
-
-plot <- ggplot()
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_t0), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag1), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag2), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag3), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag4), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag5), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag6), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag9), data = df)
-plot + geom_point(aes(x = DefFlag, y = NotionalValue_lag12), data = df)
-
-
-plot + geom_point(aes(x = DefFlag, y = DPD_lag2), data = df)
 
 m1 <- glm(DefFlag ~ GEO_region+Monthly_Income+Age+Job_type_fulltime+Home_status+Credit_amount+Number_of_installments+DPD_t0                +DPD_lag1               +DPD_lag2             +DPD_lag3 +             
           +DPD_lag7+DPD_lag9+DPD_lag10+DPD_lag12+income_ratio  , data = train, family = binomial())
@@ -100,22 +82,6 @@ summary(m)
 score <- predict(m, type = 'response', newdata = test)
 
 
-
-
-
-library(ROCR)
-
-
-g <- roc(DefFlag ~ score, data = test)
-plot(g)
-
-step(m, scale = 0,
-     direction = c("both", "backward", "forward"),
-     trace = 1, keep = NULL, steps = 4, k = 2)
-pred <- prediction(score, test$DefFlag)
-
-performance(pred,"auc")
-
 #randomForest
 
 colnames(df)
@@ -130,11 +96,6 @@ arf <- randomForest(DefFlag ~       GEO_region       +      Age        +        
                      
                     , data = na.omit(test), importance = TRUE,
                     proximity = TRUE, do.trace = T, ntree = 1000, keep.forest = TRUE)
-
-
-bs <- randomForest(DefFlag ~ .        
-                   , data = na.omit(test[,-1]), importance = TRUE,
-                   proximity = TRUE, ntree = 10, keep.forest = TRUE)
 
 varImpPlot(arf)
 importance(arf)
@@ -159,7 +120,7 @@ print(ksl.nw,condposterior=TRUE)
 
 library(corrgram)
 
-corrgram(na.omit(test), order=TRUE, lower.panel=panel.shade,
+corrgram(na.omit(test[,1:10]), order=TRUE, lower.panel=panel.shade,
          upper.panel=panel.pie, text.panel=panel.txt)
 
 
