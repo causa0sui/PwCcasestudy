35bf3753cbe61ebf420a326451d05906bfa30f1b
BartoszSzczesniak <48770904+BartoszSzczesniak@users.noreply.github.com>
2019-04-22 12:50:04 +0200

    Update model.R

--- model.R
+++ model.R
@@ -34,6 +34,7 @@ while(difftime(currTime,startTime,units="secs")<600){
      sum(variables$used)!=0 |
      runif(1) < 0.05){
     bestGini <- newGini
+    bestFormula <- formula
   }else{
     variables[changedRow,2] <- !variables[changedRow,2]
   }
@@ -41,4 +42,7 @@ while(difftime(currTime,startTime,units="secs")<600){
   iter = iter + 1
 }
 bestGini
-formula
+bestFormula
+
+#DefFlag ~  GEO_region + Job_type + Marital_status + Car_status + Household_children + Monthly_Income + Credit_amount + Agricultural_tractor_number + Building_project_subbmission_number + Employed_number_women + Employed_agricultural_number + Employed_industry_number + Emplyed_trade_transport_number + Employed_other_number + Total_population_age_0_14_years + Total_population_age_15_29_years + Spending_food + Spending_clothing + Spending_catering + Unemployed_total + Unemployed_highschool_and_lower_number + NotionalValue_lag1 + NotionalValue_lag2 + NotionalValue_lag5 + NotionalValue_lag8 + NotionalValue_lag12 + DPD_t0 + DPD_lag1 + DPD_lag2 + DPD_lag3 + DPD_lag4 + DPD_lag6 + DPD_lag7 + DPD_lag10 + NotionalOverdue_lag3 + NotionalOverdue_lag6 + NotionalOverdue_lag7 + NotionalOverdue_lag10
+#Najlepsze do tej pory, okolo 0.45
