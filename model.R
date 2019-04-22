e1202b11a2cd10f71a829bf4922f398bc648916e
BartoszSzczesniak <48770904+BartoszSzczesniak@users.noreply.github.com>
2019-04-22 14:20:24 +0200

    najlepsze zmienne
    ten kod wybiera zmienna ktora daje najwiekszy wzrost gini i dodaje ja do modelu. ale trwa to wiecznosc. niemniej jest w stanie osiagnac okolo 0.4 juz na 3 zmiennych

--- model.R
+++ model.R
@@ -8,41 +8,28 @@ variables <- data.frame(variable = names(df)[2:89], used = FALSE)
 df <- na.omit(df)
 
 smp_size <- floor(0.8 * nrow(df))
-set.seed(123)
-train_ind <- sample(seq_len(nrow(df)), size = smp_size)
 
+pool <- 1:nrow(variables)
+
+
+train_ind <- sample(seq_len(nrow(df)), size = smp_size)
 train <- df[train_ind, ]
 test <- df[-train_ind, ]
 
-bestGini <- 0
-iter <- 0
-startTime <- Sys.time()
-while(difftime(currTime,startTime,units="secs")<600){
+for (i in pool){
   
-  changedRow <- sample(nrow(variables),
-                       sample(1:2,1))
-  variables[changedRow,2] <- !variables[changedRow,2]
-  
+  variables[i,2] <- !variables[i,2]
+ 
   formula <- paste("DefFlag ~ ", 
-                   paste(variables[variables$used==TRUE,1], collapse = " + ")
-  )
+                   paste(variables[variables$used==TRUE,1], collapse = " + "))
   model <- glm(formula = formula, data = train, family = binomial())
   pred <- predict(model, type = 'response', newdata = test)
+  variables$score[i] <- Gini_value(pred, test$DefFlag)
   
-  newGini <- Gini_value(pred, test$DefFlag)
-  if(newGini > bestGini & 
-     sum(variables$used)!=0 |
-     runif(1) < 0.05){
-    bestGini <- newGini
-    bestFormula <- formula
-  }else{
-    variables[changedRow,2] <- !variables[changedRow,2]
-  }
-  currTime <- Sys.time()
-  iter = iter + 1
+  variables[i,2] <- !variables[i,2]
 }
-bestGini
-bestFormula
-
-#DefFlag ~  GEO_region + Job_type + Marital_status + Car_status + Household_children + Monthly_Income + Credit_amount + Agricultural_tractor_number + Building_project_subbmission_number + Employed_number_women + Employed_agricultural_number + Employed_industry_number + Emplyed_trade_transport_number + Employed_other_number + Total_population_age_0_14_years + Total_population_age_15_29_years + Spending_food + Spending_clothing + Spending_catering + Unemployed_total + Unemployed_highschool_and_lower_number + NotionalValue_lag1 + NotionalValue_lag2 + NotionalValue_lag5 + NotionalValue_lag8 + NotionalValue_lag12 + DPD_t0 + DPD_lag1 + DPD_lag2 + DPD_lag3 + DPD_lag4 + DPD_lag6 + DPD_lag7 + DPD_lag10 + NotionalOverdue_lag3 + NotionalOverdue_lag6 + NotionalOverdue_lag7 + NotionalOverdue_lag10
-#Najlepsze do tej pory, okolo 0.45
+  
+bestVariable <- which.max(variables$score)
+variables[bestVariable,2] <- !variables[bestVariable,2]
+pool <- c(1:(bestVariable-1),(bestVariable+1):nrow(variables))
+variables[variables$used==TRUE,]
